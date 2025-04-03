aa48c50a87672844567763d904571f87b48281b2
AminFiroozi <afiroozi007@gmail.com>
2025-04-03 21:08:59 +0330

    put api key

--- main1.py
+++ main1.py
@@ -38,7 +38,7 @@ def optifine(name):
     return "".join(Name)
 def getChat(command):
     API_URL = "https://router.huggingface.co/novita/v3/openai/chat/completions"
-    headers = {"Authorization": str(os.getenv('hfkey'))}
+    headers = {"Authorization": "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"}
     payload = {
         "messages": [
             {
